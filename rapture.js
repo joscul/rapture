330705c09e3e9a51a5a29bed5cf2d18db1c7e3cb
megaloppan <ericalovgren@hotmail.com>
2021-08-10 22:49:07 +0100

    Added keyboard events for navigation.

--- rapture.js
+++ rapture.js
@@ -6,18 +6,69 @@ class Map {
 
 }
 
-var x_pos = 0;
+var state = {
+	x_pos: 0,
+	y_pos: 0,
+	width: 100,
+	height: 100,
+	color: "green",
+	speed: 300,
+	right_pressed: false,
+	left_pressed: false,
+	up_pressed: false,
+	down_pressed: false
+}
+
 var draw = function (ctx, canvas_width, canvas_height, dt) {
-	ctx.fillStyle = 'green';
-	ctx.fillRect(x_pos, 10, 40, 40);
-	if (x_pos < 500) {
-		x_pos += 100 * dt;
+	ctx.fillStyle = state.color;
+	ctx.fillRect(state.x_pos, state.y_pos, state.width, state.height);
+	if (state.right_pressed && (state.x_pos+state.width) < canvas_width) {
+		state.x_pos += state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
+	}
+	if (state.left_pressed && state.x_pos > 0) {
+		state.x_pos -= state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
+	}
+	if (state.down_pressed && (state.y_pos+state.height) < canvas_height) {
+		state.y_pos += state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
+	}
+	if (state.up_pressed && state.y_pos > 0) {
+		state.y_pos -= state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
 	}
 
 };
 
 var clear_canvas = function (ctx, canvas_width, canvas_height) {
 	ctx.clearRect(0, 0, canvas_width, canvas_height);
+};
+
+var on_keydown = function (event) {
+	if (event.key == "ArrowRight") {
+		state.right_pressed = true;
+	}
+	if (event.key == "ArrowLeft") {
+		state.left_pressed = true;
+	}
+	if (event.key == "ArrowDown") {
+		state.down_pressed = true;
+	}
+	if (event.key == "ArrowUp") {
+		state.up_pressed = true;
+	}
+};
+
+var on_keyup = function (event) {
+	if (event.key == "ArrowRight") {
+		state.right_pressed = false;
+	}
+	if (event.key == "ArrowLeft") {
+		state.left_pressed = false;
+	}
+	if (event.key == "ArrowDown") {
+		state.down_pressed = false;
+	}
+	if (event.key == "ArrowUp") {
+		state.up_pressed = false;
+	}
 };
 
 var old_timestamp = 0;
@@ -41,12 +92,16 @@ var start_main_loop = function (canvas) {
 	var ctx = canvas.getContext("2d");
 
 	requestAnimationFrame(function (timestamp) {
+		old_timestamp = timestamp;
 		main_loop(ctx, canvas.width, canvas.height, timestamp);
 	});
 
 };
 
 var main = function () {
+	document.addEventListener("keydown", on_keydown);
+	document.addEventListener("keyup", on_keyup);
+
 	var canvas = document.createElement("canvas");
 
 	canvas.id = "Main";
@@ -62,5 +117,7 @@ var main = function () {
 
 	start_main_loop(canvas);
 };
+
 main();
+
 

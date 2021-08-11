dcc35aebc406a8426e5963bba9a6824309faae42
megaloppan <ericalovgren@hotmail.com>
2021-08-11 18:40:38 +0100

    Added more object oriented style.

--- rapture.js
+++ rapture.js
@@ -1,51 +1,84 @@
 
-class Map {
+class Player {
 
 	constructor() {
+		this.state = {
+			x_pos: 0,
+			y_pos: 0,
+			width: 100,
+			height: 100,
+			color: "green",
+			speed: 300,
+		}
+	}
+
+	draw(ctx) {
+		const {color, x_pos, y_pos, width, height} = this.state;
+		ctx.fillStyle = color;
+		ctx.fillRect(x_pos, y_pos, width, height);
+	}
+
+	update(global_state, dt) {
+		const {color, x_pos, y_pos, width, height, speed} = this.state;
+		const distance = speed * dt //Speed in pixels per second multiplied by seconds since last update
+		if (global_state.right_pressed && (x_pos + width) < global_state.canvas_width) {
+			this.state.x_pos += distance;
+		}
+		if (global_state.left_pressed && x_pos > 0) {
+			this.state.x_pos -= distance;
+		}
+		if (global_state.down_pressed && (y_pos + height) < global_state.canvas_height) {
+			this.state.y_pos += distance;
+		}
+		if (global_state.up_pressed && y_pos > 0) {
+			this.state.y_pos -= distance;
+		}
 	}
 
 }
 
-var state = {
-	x_pos: 0,
-	y_pos: 0,
-	width: 100,
-	height: 100,
-	color: "green",
-	speed: 300,
-	right_pressed: false,
-	left_pressed: false,
-	up_pressed: false,
-	down_pressed: false
+class Background {
+
+	constructor(initial_width, initial_height) {
+		this.state = {
+			color: "black",
+			width: initial_width,
+			height: initial_height
+		}
+	}
+
+	draw(ctx) {
+		const {color, width, height} = this.state;
+		ctx.fillStyle = color;
+		ctx.fillRect(0, 0, width, height);
+	}
+
+	update(global_state, dt) {
+		this.state.width = global_state.canvas_width;
+		this.state.height = global_state.canvas_height;
+		
+	}
+
 }
 
-var draw = function (ctx, canvas_width, canvas_height, dt) {
-	ctx.fillStyle = state.color;
-	ctx.fillRect(state.x_pos, state.y_pos, state.width, state.height);
+var draw = function (ctx, state, dt) {
+	state.background.draw(ctx)
+	state.player.draw(ctx)
 	ctx.fillStyle = "red";
 	ctx.beginPath();
 	ctx.arc(100, 75, 50, 0, Math.PI);
 	ctx.fill();
-	if (state.right_pressed && (state.x_pos+state.width) < canvas_width) {
-		state.x_pos += state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
-	}
-	if (state.left_pressed && state.x_pos > 0) {
-		state.x_pos -= state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
-	}
-	if (state.down_pressed && (state.y_pos+state.height) < canvas_height) {
-		state.y_pos += state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
-	}
-	if (state.up_pressed && state.y_pos > 0) {
-		state.y_pos -= state.speed * dt; //Speed in pixels per second multiplied by seconds since last update
-	}
+
+	state.player.update(state, dt);
+	state.background.update(state, dt);
 
 };
 
-var clear_canvas = function (ctx, canvas_width, canvas_height) {
-	ctx.clearRect(0, 0, canvas_width, canvas_height);
+var clear_canvas = function (ctx, state) {
+	ctx.clearRect(0, 0, state.canvas_width, state.canvas_height);
 };
 
-var on_keydown = function (event) {
+var on_keydown = function (event, state) {
 	if (event.key == "ArrowRight") {
 		state.right_pressed = true;
 	}
@@ -60,7 +93,7 @@ var on_keydown = function (event) {
 	}
 };
 
-var on_keyup = function (event) {
+var on_keyup = function (event, state) {
 	if (event.key == "ArrowRight") {
 		state.right_pressed = false;
 	}
@@ -77,34 +110,45 @@ var on_keyup = function (event) {
 
 var old_timestamp = 0;
 var fps = 0;
-var main_loop = function (ctx, canvas_width, canvas_height, timestamp) {
+var main_loop = function (ctx, state, timestamp) {
 
 	var dt = (timestamp - old_timestamp) / 1000;
 	old_timestamp = timestamp;
 
 	fps = Math.round(1.0 / dt);
 
-	clear_canvas(ctx, canvas_width, canvas_height);
-	draw(ctx, canvas_width, canvas_height, dt);
+	clear_canvas(ctx, state);
+	draw(ctx, state, dt);
 
 	requestAnimationFrame(function (timestamp) {
-		main_loop(ctx, canvas_width, canvas_height, timestamp);
+		main_loop(ctx, state, timestamp);
 	});
 }
 
 var start_main_loop = function (canvas) {
 	var ctx = canvas.getContext("2d");
+	var state = {
+		right_pressed: false,
+		left_pressed: false,
+		up_pressed: false,
+		down_pressed: false,
+		canvas_width: canvas.width,
+		canvas_height: canvas.height,
+		player: new Player(),
+		background: new Background(canvas.width, canvas.height)
+	}
+
+	document.addEventListener("keydown", (event) => on_keydown(event, state));
+	document.addEventListener("keyup", (event) => on_keyup(event, state));
 
 	requestAnimationFrame(function (timestamp) {
 		old_timestamp = timestamp;
-		main_loop(ctx, canvas.width, canvas.height, timestamp);
+		main_loop(ctx, state, timestamp);
 	});
 
 };
 
 var main = function () {
-	document.addEventListener("keydown", on_keydown);
-	document.addEventListener("keyup", on_keyup);
 
 	var canvas = document.createElement("canvas");
 
